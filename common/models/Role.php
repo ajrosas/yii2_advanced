550016bcb70b5e91472879a13dea40c2f619dc1c
keiffer-filweb <68670839+keiffer-filweb@users.noreply.github.com>
2022-07-28 09:16:13 +0800

    created role model
    Create Role.php in common to access by either Frontend or Backend

---
+++ common/models/Role.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%role}}".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string|null $module_access
+ * @property string|null $role_access
+ * @property string|null $navigations
+ * @property int|null $level
+ * @property int $status
+ * @property string|null $created_at
+ * @property string|null $updated_at
+ */
+class Role extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%role}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['module_access', 'role_access', 'navigations'], 'string'],
+            [['level', 'status'], 'integer'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['name'], 'string', 'max' => 255],
+            [['name'], 'unique'],
+            [['level'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'module_access' => 'Module Access',
+            'role_access' => 'Role Access',
+            'navigations' => 'Navigations',
+            'level' => 'Level',
+            'status' => 'Status',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+}
