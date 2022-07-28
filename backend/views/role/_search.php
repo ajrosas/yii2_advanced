97544cc0fd89a1c07c83c48dc5bab9e6930a8262
keiffer-filweb <68670839+keiffer-filweb@users.noreply.github.com>
2022-07-28 09:22:53 +0800

    added CRUD Role in backend

---
+++ backend/views/role/_search.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\search\RoleSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="role-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'name') ?>
+
+    <?= $form->field($model, 'module_access') ?>
+
+    <?= $form->field($model, 'role_access') ?>
+
+    <?= $form->field($model, 'navigations') ?>
+
+    <?php // echo $form->field($model, 'level') ?>
+
+    <?php // echo $form->field($model, 'status') ?>
+
+    <?php // echo $form->field($model, 'created_at') ?>
+
+    <?php // echo $form->field($model, 'updated_at') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-outline-secondary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ backend/views/role/index.php
@@ -0,0 +1,50 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel backend\models\search\RoleSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Roles';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="role-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Role', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'name',
+            'module_access:ntext',
+            'role_access:ntext',
+            'navigations:ntext',
+            //'level',
+            //'status',
+            //'created_at',
+            //'updated_at',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, Role $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+
+</div>
